2f35dac6a3b2ce889a339f69798dc162f9aec139
VenusCoin <venuscoin@yahoo.com>
2017-06-12 02:36:33 +0200

    Add files via upload

---
+++ VNCmaster/scr/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define venuscoin-Qt message queue name
+#define venuscoinURI_QUEUE_NAME "venuscoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
